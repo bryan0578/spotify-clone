979886f6bc7437c9ea05faf037410d33aea37d74
Bryan <bryan.cash78@gmail.com>
2023-08-30 18:38:00 -0400

    image fix

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -36,7 +36,7 @@ const Liked = async () => {
               <Image
                 className="object-cover"
                 fill
-                src="/images/liked.png"
+                src="/Images/liked.png"
                 alt="Playlist"
               />
             </div>
